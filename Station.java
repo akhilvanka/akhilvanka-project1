57572a6db6544c55c49e3752ca89c8aafa510014
Akhil Vanka <dev+github@akhilv.me>
2024-10-27 22:36:42 -0400

    Implemented Station and Transfer Station classes

---
+++ Station.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+
+public class Station {
+    protected String line_color; 
+    protected String station_name;
+    protected Station next; 
+    protected Station prev;
+    protected Boolean service_availability;
+
+    public Station(String line_color, String station_name){
+        this.line_color = line_color;
+        this.station_name = station_name;
+        this.next = null;
+        this.prev = null;
+        this.service_availability = true;
+    }
+
+    public String getLineColor(){
+        return this.line_color;
+    }
+
+    public String getStationName(){
+        return this.station_name;
+    }
+
+    public Station getNextStation(){
+        return this.next;
+    }
+
+    public Station getPrevStation(){
+        return this.prev;
+    }
+
+    public void addNext(Station next_station){
+        this.next = next_station;
+        if (next.prev == null) {
+            next.prev = this;
+        }
+    }
+
+    public void addPrev(Station prev_station){
+        this.prev = prev_station;
+        if (prev.next == null) {
+            prev.next = this;
+        }
+    }
+
+
+    // Connect 
+    public void connect(Station other) {
+        if (other == null) {
+            return;
+        }
+ 
+        this.addNext(other);
+        other.addPrev(this);
+    }
+
+    public Boolean isAvailable(){
+        return this.service_availability;
+    }
+    
+    public void switchAvailable() {
+        this.service_availability = !this.service_availability;
+    }
+
+    public String toString(){
+        return "STATION " + station_name + ": " + line_color + " line, in service: " + (service_availability ? "true" : "false") + ", previous station: " + (prev == null ? "none": prev.getStationName()) + ", next station: " + (next == null ? "none": next.getStationName());
+    }
+
+    public Boolean equals(Station other){
+        return this.line_color.equals(other.getLineColor()) && this.station_name.equals(other.getStationName());
+    }
+
+    public int tripLength(Station other) {
+        ArrayList<Station> visitedStations = new ArrayList<Station>();
+        return tripLength(other, visitedStations);
+    }
+
+    private int tripLength(Station other, ArrayList<Station> visitedStations) {
+        if (this.equals(other)) {
+            return 0;
+        }
+        visitedStations.add(this);
+        if (this.next != null && !visitedStations.contains(this.next)) {
+            int nextLength = this.next.tripLength(other, visitedStations);
+            if (nextLength != -1) {
+                return 1 + nextLength;
+            }
+        }
+
+        if (this instanceof TransferStation) {
+            for (Station station : ((TransferStation)this).otherStations) {
+                if (!visitedStations.contains(station)) {
+                    int nextLength = station.tripLength(other, visitedStations);
+                    if (nextLength != -1) {
+                        return 1 + nextLength;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ TransferStation.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class TransferStation extends Station {
+    public ArrayList<Station> otherStations;
+
+    public TransferStation(String line_color, String station_name){
+        super(line_color, station_name);
+        this.otherStations = new ArrayList<Station>();
+    }
+
+    public void addTransferStationNext(Station station) {
+        if (station == null) {
+            return;
+        }
+
+        this.otherStations.add(station);
+        station.prev = this;
+    }
+
+    public void addTransferStationPrev(Station station) {
+        if (station == null) {
+            return;
+        }
+
+        this.otherStations.add(station);
+        station.next = this;
+    }
+
+
+    public String toString() {
+        String result = "TRANSFER" + super.toString() + "\n\tTransfers: \n";
+        for (Station station : this.otherStations) {
+            result += "\t" + station.toString() + "\n";
+        }
+        return result;
+    }
+}
